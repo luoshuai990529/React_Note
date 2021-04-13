ef145c500a824066512cb8be518336a435613233
luoshuai <1029113525@qq.com>
2021-04-13 19:38:15 +0800

    feat:usestate补充惰性初始化state

--- src/page/hooks-study/useState.js
+++ src/page/hooks-study/useState.js
@@ -21,21 +21,32 @@ import { useState } from "react";
 // }
 
 function App() {
-  // 推荐写法
-  // 注意点：useState不能存在于条件判断语句中
-  const [cartNum, setCartNumState] = useState(10);
-  const [userName, setUserNameState] = useState('游客');
-  // 状态的三部分：初始化，获取和修改
-  // const aa = useState(100);  //   用于状态初始化
-  // const cartNum = aa[0];     //   获取状态的
-  // const setState = aa[1];    //   用于修改状态的
-  // console.log(aa);
-  return <>
-    <h1>用户：{userName}</h1>
-    <h2>购物车数量：{cartNum}</h2>
-    <button onClick={() => setCartNumState(cartNum + 1)}>数量加1</button>
-    <button onClick={() => setUserNameState('小白')}>点击登录</button>
-  </>;
+    // 推荐写法
+    // 注意点：useState不能存在于条件判断语句中
+    const [cartNum, setCartNumState] = useState(10);
+    const [userName, setUserNameState] = useState('游客');
+    // 状态的三部分：初始化，获取和修改
+    // const aa = useState(100);  //   用于状态初始化
+    // const cartNum = aa[0];     //   获取状态的
+    // const setState = aa[1];    //   用于修改状态的
+    // console.log(aa);
+
+    // 惰性初始化state 这个函数只会在初始时会执行一次
+    function getInitState() {
+        let initState = 0;
+        for (let i = 0; i < 100; i++) {
+            initState += i
+        }
+        return initState
+    }
+    const [initstate, setInitState] = useState(getInitState)
+
+    return <>
+        <h1>用户：{userName}</h1>
+        <h2>购物车数量：{cartNum}</h2>
+        <button onClick={() => setCartNumState(cartNum + 1)}>数量加1</button>
+        <button onClick={() => setUserNameState('小白')}>点击登录</button>
+    </>;
 }
 
 

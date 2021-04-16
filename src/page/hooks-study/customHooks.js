666fc8001b3a734194b0ecf05ce01dd8f52fb138
luoshuai <1029113525@qq.com>
2021-04-16 19:01:20 +0800

    tips补充

--- src/page/hooks-study/customHooks.js
+++ src/page/hooks-study/customHooks.js
@@ -29,6 +29,7 @@ function App(){
     const size = useWinSize();
     
     return <>
+        <h1 className='desc'> 自定义 Hook 必须以 use 开头,不遵循的话，由于无法判断某个函数是否包含对其内部 Hook 的调用，React 将无法自动检查你的 Hook 是否违反了 Hook 的规则。</h1>
         <h1>现在页面的大小是：{size.width}*{size.height}</h1>
     </>
 }
